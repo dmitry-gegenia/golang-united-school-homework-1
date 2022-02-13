ecda215ee0442fbc3b2881b9c5d023092c6e663c
Dmitry Gegenia <dmitry@gegenia.dev>
2022-02-13 10:28:59 +0300

    edited solution

--- solution.go
+++ solution.go
@@ -1,15 +1,9 @@
-package main
+package solution
 
 import (
-	"fmt"
-
 	"github.com/kyokomi/emoji/v2"
 )
 
 func GetMessage() string {
 	return emoji.Sprint("Hello :world_map:!")
-}
-
-func main() {
-	fmt.Println(GetMessage())
 }
